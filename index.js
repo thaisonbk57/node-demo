95c3fa784632fe2a437fca882803d330b4c4441a
Thai Son Dang <thaisondang.bk57@gmail.com>
2018-11-22 23:38:21 +0100

    initialize the project

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(port, () => {
+  console.log(`listening to port ${port}...`);
+});
